141ec07138a97f1547f3a49ac75f688c66f8ea53
Christos Bramis <bramis04chris@gmail.com>
2025-10-26 17:06:11 +0200

    initialization

---
+++ bram-vortex/src/main/java/com/christos_bramis/bram_vortex/BramVortexApplication.java
@@ -0,0 +1,13 @@
+package com.christos_bramis.bram_vortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BramVortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BramVortexApplication.class, args);
+	}
+
+}
---
+++ bram-vortex/src/test/java/com/christos_bramis/bram_vortex/BramVortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christos_bramis.bram_vortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BramVortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
